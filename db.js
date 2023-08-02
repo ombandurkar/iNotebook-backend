764c3c38382a63c8342a808fa23f6dc67e430b9c
Om Bandurkar <89293380+ombandurkar@users.noreply.github.com>
2023-08-02 13:58:13 +0530

    Update db.js with mongoURI as env variable

--- db.js
+++ db.js
@@ -1,9 +1,8 @@
 //database se connenct hone waala code hum yaha par likhege
 
 const mongoose = require('mongoose');
-// const mongoURI = "mongodb://localhost:27017/" -> by using this, youu app crashes within every 1 min if you leave it idle
-const mongoURI = "mongodb://127.0.0.1:27017/iNotebooK"
-
+require('dotenv').config()
+const mongoURI = process.env.MONGO_URL
 const connectToMongo = () => {
     mongoose.connect(mongoURI);
     console.log("Conncted to Mongo sucessfully!");
